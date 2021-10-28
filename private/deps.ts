49f1f03ecc6dbf667e1b4c3712b0109e316c0f1d
jeremiah <jeremiah.shaulov@gmail.com>
2021-10-29 02:16:44 +0300

    To jstok@v1.0.1

--- private/deps.ts
+++ private/deps.ts
@@ -1,1 +1,1 @@
-export {jstok, jstokReader, Token, TokenType} from 'https://deno.land/x/jstok@v0.1.4/mod.ts';
+export {jstok, jstokReader, Token, TokenType} from 'https://deno.land/x/jstok@v1.0.1/mod.ts';
